de8f9959aee639add4230ddf87f1913333cb01dd
Tolfinho <tolfojoao2@gmail.com>
2025-11-25 07:37:33 -0300

    Adiciona estilizações e página de exercícios

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 import Navbar from './components/Navbar.vue'
 
 export default {
-    components: { Navbar }
+  components: { Navbar }
 }
 </script>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,47 @@
 <template>
-    <nav class="navbar">
-        <router-link to="/">Home</router-link>
-        <router-link to="/treinos">Treinos</router-link>
-        <router-link to="/adicionar-treino">Novo treino</router-link>
-        <router-link to="/exercicios">Exercícios</router-link>
-        <router-link to="/perfil">Perfil</router-link>
-    </nav>
+    <div class="main-container">
+        <div class="container">
+            <div class="logo-container">
+                <h2>CalisTracker</h2>
+            </div>
+            <nav class="navbar">
+                <router-link to="/">Home</router-link>
+                <router-link to="/treinos">Treinos</router-link>
+                <router-link to="/adicionar-treino">Novo treino</router-link>
+                <router-link to="/exercicios">Exercícios</router-link>
+                <router-link to="/perfil">Perfil</router-link>
+            </nav>
+        </div>
+    </div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+.main-container {
+    height: 60px;
+    background-color: var(--primary1);
+    color: var(--secondary1);
+}
+
+.container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+}
+
+.navbar {
+    display: flex;
+    gap: 30px;
+}
+
+.navbar a {
+    color: var(--secondary1);
+    text-decoration: none;
+}
+
+.navbar a:hover {
+    color: var(--secondary2);
+}
+</style>
